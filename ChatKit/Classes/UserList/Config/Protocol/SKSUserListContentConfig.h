3f6923d0c86aa078d215b2601b7268895d2e5174
iCrany <crany1992@gmail.com>
2017-10-17 14:26:48 +0800

    Remove UserList logic

--- ChatKit/Classes/UserList/Config/Protocol/SKSUserListContentConfig.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by iCrany on 2016/11/9.
-// Copyright (c) 2016 Sachsen. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-@protocol SKSUserListContentConfig <NSObject>
-@end
